1e7de800ed632b3e08e10d9fa55733b4af0551bf
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-19 18:08:17 -0300

    adjusted the main component

--- GeradorQRCode/src/Components/InputQRcode/InputQRcode.jsx
+++ GeradorQRCode/src/Components/InputQRcode/InputQRcode.jsx
@@ -1,61 +1,83 @@
-import { useRef, useState } from 'react';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import KeyboardTwoToneIcon from '@mui/icons-material/KeyboardTwoTone';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { styled } from '@mui/system';
-import QRCode from 'react-qr-code';
+import { useRef, useState, useCallback, useEffect } from 'react';
+import QRCodeLink from 'qrcode';
 
-const StyledKeyboardTwoToneIcon = styled(KeyboardTwoToneIcon)({
-  cursor: 'pointer',
-  transition: 'color 0.3s ease-in-out',
-  '&:hover': {
-    color: 'black'
-  },
-});
+//Components
+import CustomTextField from './subComponents/CustomTextField';
+import QRCodeCustom from './subComponents/QRCodeCustom';
+import CustomButton from './subComponents/CustomButton';
+import CustomToast from './subComponents/CustomToast';
 
 function InputQRcode() {
   const textQRcodeRef = useRef(null);
   const [link, setLink] = useState('');
+  const [qrCodeLink, setQrCodeLink] = useState('');
+  const [toastShown, setToastShown] = useState(false);
 
-  const handleIconClick = () => {
-    if (textQRcodeRef.current) {
-      textQRcodeRef.current.focus();
+  const generateQrCode = useCallback((link_url) => {
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function (err, url) {
+      if (err) {
+        return
+      } else {
+        setQrCodeLink(url);
+      }
+    });
+  }, []);
+
+  const isDisabled = link === '';
+
+  const showToast = useCallback(() => {
+    if (!toastShown) {
+      setToastShown(true);
+      setLink('');
+      setTimeout(() => {
+        setToastShown(false);
+      }, 1)
+    } else {
+      return
     }
-  };
+  }, [toastShown]);
+
+  const handleButtonClick = useCallback(() => {
+    if (!isDisabled) {
+      showToast();
+      const linkElement = document.createElement('a');
+      linkElement.href = qrCodeLink;
+      linkElement.download = 'qrcode.png';
+      document.body.appendChild(linkElement);
+      linkElement.click();
+      document.body.removeChild(linkElement);
+    }
+  }, [isDisabled, showToast, qrCodeLink]);
+
+  useEffect(() => {
+    if (link !== '') {
+      generateQrCode(link);
+      setToastShown(false);
+    }
+  }, [link, generateQrCode]);
 
   return (
+    // all components are in subComponents
     <>
-      <QRCode
-        value={link}
-      />
+      <CustomToast showToast={toastShown} />
 
-      <TextField
+      <QRCodeCustom value={link} />
+
+      <CustomTextField
         inputRef={textQRcodeRef}
-        id="textQRcode"
-        label="Digite um texto"
-        placeholder='Digite um texto...'
-        multiline
-        style={{ cursor: 'pointer' }}
-        maxRows={6}
         value={link}
-        onChange={(event) => {
-          setLink(event.target.value);
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment
-              position="start"
-              style={{ cursor: 'pointer' }}
-              onClick={handleIconClick}
-            >
-              <StyledKeyboardTwoToneIcon />
-            </InputAdornment>
-          ),
-        }}
-        variant="outlined"
+        onChange={(event) => setLink(event.target.value)}
+        onKeyDown={(event) => (event.key === 'Enter' ? (event.preventDefault(), handleButtonClick()) : null)}
       />
 
+      <CustomButton
+        url={qrCodeLink}
+        disabled={isDisabled}
+        onClick={handleButtonClick}
+      />
     </>
   )
 }
