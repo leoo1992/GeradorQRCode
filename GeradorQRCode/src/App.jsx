884fb112a09dd4adff1dfcb520174e9f6cdfbcf0
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-19 08:26:46 -0300

    add component in app.jsx

--- GeradorQRCode/src/App.jsx
+++ GeradorQRCode/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import './App.css';
+import InputQRcode from './Components/InputQRcode/InputQRcode';
 
 function App() {
 
 
   return (
     <>
-     
+      <div className='d-flex flex-column justify-content-center align-items-center align-content-center vh-100 p-0 m-0 gap-4'>
+        <InputQRcode />
+      </div>
     </>
   )
 }
