bc521a791276296f71bc89006141fc807e2dfb92
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-19 18:05:27 -0300

    add prime react UI

--- GeradorQRCode/src/App.jsx
+++ GeradorQRCode/src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css';
 import InputQRcode from './Components/InputQRcode/InputQRcode';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import { PrimeReactProvider } from "primereact/api";
 
 function App() {
 
-
   return (
-    <>
+    <PrimeReactProvider>
       <div className='d-flex flex-column justify-content-center align-items-center align-content-center vh-100 p-0 m-0 gap-4'>
         <InputQRcode />
       </div>
-    </>
+    </PrimeReactProvider>
   )
 }
 
