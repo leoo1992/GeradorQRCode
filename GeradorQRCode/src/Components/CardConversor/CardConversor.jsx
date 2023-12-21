e8314ba32feacfc146425013d50667c70a49736a
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-21 17:47:37 -0300

    theme Switcher

--- GeradorQRCode/src/Components/CardConversor/CardConversor.jsx
+++ GeradorQRCode/src/Components/CardConversor/CardConversor.jsx
@@ -16,7 +16,7 @@ function CardConversor() {
             className={`m-0 p-0 d-flex text-center shadow-lg opacity-85 ${!isSwitchChecked ? 'bg-purple-900' : ''}`}
             style={cardStyle}>
             <div className='text-end d-flex justify-content-end align-content-end align-items-end m-0 p-0'>
-                <ThemeSwitch className='m-0 p-0' onSwitchChange={(checked) => setIsSwitchChecked(!checked)} />
+                <ThemeSwitch className='m-0 p-0' onSwitchChange={(checked) => setIsSwitchChecked(!checked)} defaultChecked={isSwitchChecked} />
             </div>
             <CardTitle />
             <CardBody />
