9ce7f1b4bb3622249185eebc9dcb89cd96489654
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-21 17:44:56 -0300

    error logic

--- GeradorQRCode/src/index.jsx
+++ GeradorQRCode/src/index.jsx
@@ -1,10 +1,24 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ErrorPage from './ErrorPage.jsx';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+console.error = () => {
+    console.log(`Comportamento não previsto`);
+};
+console.warn = () => {
+    console.log(`Comportamento não previsto`);
+};
+
+try {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <App />
+    );
+} catch (error) {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <ErrorPage />
+    );
+}
