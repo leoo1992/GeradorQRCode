ec711425f9a4b4b9472ecf26b66ec91f0567c142
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-12-22 17:29:00 -0300

    simplified the app.jsx

--- GeradorQRCode/src/App.jsx
+++ GeradorQRCode/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import CardConversor from './Components/CardConversor/CardConversor';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import { PrimeReactProvider } from 'primereact/api';
 import Button from '@mui/material/Button';
-import BackgroundImage from './Assets/galaxy.jpg';
-import ErrorPage from './ErrorPage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import GeneratorPage from './pages/Generator/GeneratorPage';
 
 function App() {
   const [showErrorPage, setShowErrorPage] = useState(false);
@@ -32,22 +31,7 @@ function App() {
           </Button>
         </>
       ) : (
-        <>
-          <div className="fundo-pagina d-flex justify-content-center align-items-center align-content-center vh-100 w-100">
-            <img src={BackgroundImage} alt="Background" className="background-image" />
-          </div>
-          <div className='d-flex justify-content-center align-items-center align-content-center vh-100 w-100 p-0 m-0 z-1'>
-            <CardConversor />
-            <Button
-              variant="contained"
-              color="secondary"
-              className='d-none position-absolute bottom-0 end-0 z-2 opacity-50'
-              onClick={handleClick}
-            >
-              404 Error
-            </Button>
-          </div>
-        </>
+        <GeneratorPage handleClick={handleClick} />
       )}
     </PrimeReactProvider>
   );
